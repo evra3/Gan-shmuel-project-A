c0e83fb17cdf6bb1c72407fd1e5e3eddb9d15983
Ori Meyuhas <orimeyuhas@gmail.com>
2022-06-20 13:32:24 +0300

    Adding flask app

---
+++ devops/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def home():
+    return "OK", 200
+
+@app.route('/health', methods = ['GET'])
+def health():
+    return "OK", 200
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
